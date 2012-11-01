d79b3327e6eaf425431297e5f8e86125cf207f34
Greg Burek <greg.burek@gmail.com>
2012-10-31 21:56:37 -0700

    Adds setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+ln -s ./mutt ~/.mutt
+ln -s ./offlineimaprc ~/.offlineimaprc
+ln -s ./urlview ~/.urlview
+
+ln -s ./vim ~/.vim
+
+ln -a ./tmux.conf ~/.tmux.conf
+
+ln -s ./zsh ~/.zsh
+
 
