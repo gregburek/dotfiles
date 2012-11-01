cf6276f5ac7b1eb67ef27d86d232ca2a1f3e1f06
Greg Burek <greg.burek@gmail.com>
2012-10-31 22:09:25 -0700

    Adds pwd to link path and vim zsh rc files

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-ln -s ./mutt ~/.mutt
-ln -s ./offlineimaprc ~/.offlineimaprc
-ln -s ./urlview ~/.urlview
+ln -s $(pwd)/../mutt ~/.mutt
+ln -s $(pwd)/../offlineimaprc ~/.offlineimaprc
+ln -s $(pwd)/../urlview ~/.urlview
 
-ln -s ./vim ~/.vim
+ln -s $(pwd)/../vim ~/.vim
+ln -s $(pwd)/../vim/vimrc ~/.vimrc
 
-ln -s ./tmux.conf ~/.tmux.conf
+ln -s $(pwd)../tmux.conf ~/.tmux.conf
 
-ln -s ./zsh ~/.zsh
+ln -s $(pwd)/../zsh ~/.zsh
+ln -s $(pwd)/../zsh/zshrc ~/.zshrc
 
 
